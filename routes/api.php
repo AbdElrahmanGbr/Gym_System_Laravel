6d3eff03cf29d8f12631cba1ccde1392f49a0908
omaimamahmoud244@gmail.com <your e-mail>
2022-05-02 12:36:41 +0200

    Finished Sessions Part

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RemainingTrainingSessionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/attendance/history',[RemainingTrainingSessionsController::class, 'show']);
+
+Route::get('/session/remaining', [RemainingTrainingSessionsController::class, 'remainingSession']);
+
+Route::post('training-sessions/{id}/attend', [RemainingTrainingSessionsController::class, 'attendSession']);
