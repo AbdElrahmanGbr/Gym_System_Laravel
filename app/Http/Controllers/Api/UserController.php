ef0cfb8fcaed81bb99374fabfa3d2e501c0e4b5f
shimaa-00 <shimaakhallaf00@gmail.com>
2022-05-02 18:33:43 +0200

    adding user request

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -66,7 +66,6 @@ class UserController extends Controller
 
     public function update(Request $request)
     {
-
     }
 
     //=========================================================================/
