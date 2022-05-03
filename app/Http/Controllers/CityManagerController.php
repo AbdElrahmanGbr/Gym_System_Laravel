44fed907d831c42b1dd149e95feda370d5fac720
Mohamed-Shhata <mohamed01068215253@gmail.com>
2022-05-03 10:33:52 +0200

    fix some errors

--- app/Http/Controllers/CityManagerController.php
+++ app/Http/Controllers/CityManagerController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Staff;
 use App\Models\City;
-use Illuminate\Http\Request;
+
 
 class CityManagerController extends Controller
 {
