b267f640e1cd8e33f29683c1804f0c4ac28cc619
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-05-06 22:28:40 +0200

    fixing edit from gym manager

--- resources/views/gym-managers/edit.blade.php
+++ resources/views/gym-managers/edit.blade.php
@@ -6,30 +6,30 @@
 @section('content')
 <div class=" mydiv">
 
-    <form method="post" action="{{route('gym-managers.update',$staff->id)}}"
+    <form method="post" action="{{route('gym-managers.update',$user->id)}}"
         class="row d-flex flex-column justify-content-center align-items-center" enctype="multipart/form-data">
 
         @csrf
         @method('PUT')
         <div class="text-center">
             <label for="avatar" class="form-label" role="button">
-                <img class="profile-user-img img-fluid img-circle" src="{{asset('images/'. $staff->avatar . '')}}"
+                <img class="profile-user-img img-fluid img-circle" src="{{asset('images/'. $user->avatar . '')}}"
                     alt="User profile picture">
             </label>
-            <input type="file" name="avatar" id="avatar" class="d-none" value="{{$staff->avatar}}"
+            <input type="file" name="avatar" id="avatar" class="d-none" value="{{$user->avatar}}"
                 accept="image/x-png,image/gif,image/jpeg" />
         </div>
 
 
-        <input type="hidden" name="id" value="{{ $staff->id }}">
+        <input type="hidden" name="id" value="{{ $user->id }}">
         <div class="mb-3 col-sm-6">
             <label for="Name" class="form-label">Name</label>
             <input type="text" class="form-control" id="name" name="name" aria-describedby="emailHelp"
-                value="{{$staff->name}}" />
+                value="{{$user->name}}" />
         </div>
         <div class="mb-3 col-sm-6">
             <label for="Email" class="form-label">Email</label>
-            <input type="email" name="email" id="Email" class="form-control" value="{{$staff->email}}" />
+            <input type="email" name="email" id="Email" class="form-control" value="{{$user->email}}" />
         </div>
         <div class="mb-3 col-sm-6">
             <label for="pass" class="form-label">Old Password</label>
@@ -46,7 +46,7 @@
         <div class="mb-3 col-sm-6">
             <label for="national_id" class="form-label">National_id</label>
             <input type="text" name="national_id" id="national_id" class="form-control"
-                value="{{$staff->national_id}}" />
+                value="{{$user->national_id}}" />
         </div>
 
 
