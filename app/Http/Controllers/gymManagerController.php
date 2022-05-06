ee01d940343ab4f261ba4d6d8b6789580cb8ad28
omaimataha2 <omaimamahmoud244@gmail.com>
2022-05-06 19:45:40 +0200

    finished listing,editing, and deleting  Gym Manager

--- app/Http/Controllers/gymManagerController.php
+++ app/Http/Controllers/gymManagerController.php
@@ -3,44 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Staff;
+// use App\Models\Staff;
 use App\Models\City;
 use App\Models\Gym;
 use App\Models\GymManager;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class gymManagerController extends Controller
 {
-    public function index()
-    {
+    public function index() {
+        $gymManagers = User::role('gym_manager')->get();
+
+        if (Auth::user()->hasRole('city_manager')) {
+            $gyms = City::where('user_id', Auth::user()->id)->first()->gyms;
+            $gymManagers = collect();
+            foreach ($gyms as $gym) {
+                $gymMangs = $gym->gymManager;
+                foreach ($gymMangs as $manager) {
+                    $gymManagers->push($manager);
+                }
+            }
+        }
         if (request()->ajax()) {
-            return datatables()->of(User::where('role', 'gym_manager')->get())
+            return datatables()->of($gymManagers)
                 ->addColumn('action', function ($data) {
+                    $banStatus = $data->isBanned() ? "unban" : "ban";
+                    $buttonColor = $data->isBanned() ? "secondary" : "warning";
                     $button = '<a href="' . route('gym-managers.edit', $data->id) . '" class="btn btn-info btn-sm mx-2">Edit</a>';
                     $button .= '<a href="javascript:void(0);" onClick = "deleteFunc(' . $data->id . ')"class="btn btn-danger btn-sm mx-2">Delete</a>';
+                    $button .= '<a href="javascript:void(0);" onClick = "ban(' . $data->id . ')"class="btn btn-' . $buttonColor . ' btn-sm mx-2">' . $banStatus . '</a>';
                     return $button;
+                })
+                ->addColumn('gym-city', function ($data) {
+                    $gymManage = GymManager::where('user_id', $data->id)->first();
+                    if(isset($gymManage))
+                    {
+                    $gym = Gym::find($gymManage->gym_id);
+                    $city = City::find($gym->city_id);
+                    return $gym->name . '-' . $city->name;
+                    }
+                    return "None";
+
                 })
                 ->rawColumns(['action'])->make(true);
         }
         return view('gym-managers.index');
     }
-    //--------------------------- edit User member -----------------------
-    public function edit($userId)
-    {
 
-        $user = User::find($userId);
-        $gymId = gymManager::where('user_id', $userId)->first()->gym_id;
-        //dd($gymId);
-        //$gymId = $gymMan->gym_id;
-        $gym = Gym::where('id', $gymId)->first();
-        //dd($gym);
+     //--------------------------- edit user member -----------------------
+     public function edit($user_id) {
+
+        $user_id = User::find($user_id);
+        $gymId = gymManager::where('user_id', $user_id)->first()->gym_id;
+        $gym = Gym::find($gymId);
+        $cityGyms = $gym->city->gyms;
         $cities = City::all();
         $gyms = Gym::all();
+
         return view('gym-managers.edit', [
-            'user' => $user,
+            'user_id' => $user_id,
             'gyms' => $gyms,
             'cities' => $cities,
-            'gym' => $gym
+            'gym' => $gym,
+            'cityGyms' => $cityGyms
 
         ]);
     }
@@ -110,10 +136,15 @@ class gymManagerController extends Controller
     }
     //-------------------- delete member -------------------------------
 
-    public function destroy(Request $request)
-    {
+    public function destroy(Request $request) {
 
         $member = User::where('id', $request->id)->delete();
         return Response()->json($member);
+    }
+    public function ban(Request $request) {
+
+        $member = User::find($request->id);
+        $ban = $member->isBanned() ? $member->unban() : $member->ban();
+        return Response()->json($ban);
     }
 }
